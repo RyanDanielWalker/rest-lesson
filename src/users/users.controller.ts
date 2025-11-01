0f3a9fcd7f017bd9a4ee387aae5d676f9f5499e7
Ryan Walker <ryandanielwalker@gmail.com>
2025-11-01 15:55:11 -0400

    users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() // GET /users or /users?role=value&age=value {<- query parameters}
+  findAll(@Query('role') role?: string, @Query('age') age?: number) {
+    return [];
+  }
+
+  @Get('interns') // static route should be before dynamic route
+  findInterns() {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: any) {
+    return user;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() user: any) {
+    return { id };
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return { id };
+  }
+}
